bbf2c4eaf89859398b2ec726e1eb47c49ad2bbb5
Aprameya A Rao <rao.aprameya1205@gmail.com>
2025-05-19 19:49:10 +0530

    day 2.1

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint,render_template  #to import templates need to import render_template
 
 views=Blueprint('views',__name__)
 
 
 @views.route('/') #for home page
 def home():
-    return "hqmepage"
+    return render_template("home.html")
